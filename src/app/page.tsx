a67cac2c771d9f19f312c0216fac0e87ffa42089
kmjak <tymkoron1221@icloud.com>
2025-02-12 17:19:27 +0900

    /ページを作った

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,11 @@
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <main>
+    <main className="flex justify-center items-center h-full w-full">
+      <Link href="/game">
+        <p className="text-3xl font-semibold px-3 py-2 border rounded-md border-gray hover:bg-gray hover:text-background hover:border-none transition-colors">Play Gravity Shapes</p>
+      </Link>
     </main>
   );
 }
