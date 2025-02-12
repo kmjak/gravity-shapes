d1de378b9de68caa261371901ab14a979d7ff644
kmjak <tymkoron1221@icloud.com>
2025-02-12 17:39:58 +0900

    blockとshpeの型定義ファイルを作った

---
+++ src/types/block.ts
@@ -0,0 +1,6 @@
+interface Block {
+  x: number;
+  y: number;
+}
+
+export default Block;
---
+++ src/types/shape.ts
@@ -0,0 +1,11 @@
+import Block from "@/types/block";
+
+interface Shape {
+  id: number;
+  blocks: Block[]
+  current_x: number;
+  current_y: number;
+  color: string;
+}
+
+export default Shape;
