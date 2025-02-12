c4dde6057d0d45287be08dd0b4480493425fe943
kmjak <tymkoron1221@icloud.com>
2025-02-13 01:00:57 +0900

    数字をランダムに生成する時にgenerateRandomNumberを使う様に変更した

--- src/utils/shuffleArray.ts
+++ src/utils/shuffleArray.ts
@@ -1,7 +1,9 @@
+import generateRandomNumber from "./generateRandomNumber";
+
 export default async function shuffleArray(array: number[]): Promise<number[]> {
   const shuffled:number[] = [...array];
   for (let i:number = shuffled.length - 1; i > 0; i--) {
-    const j:number = Math.floor(Math.random() * (i + 1));
+    const j:number = await generateRandomNumber({min: 0, max: i});
     [shuffled[i], shuffled[j]] = [shuffled[j], shuffled[i]];
   }
   return shuffled;
