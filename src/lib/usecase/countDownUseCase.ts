190048c0e23dbdb260794235f263efa5f18bbaa2
kmjak <tymkoron1221@icloud.com>
2025-02-14 02:36:51 +0900

    カウントダウの秒数をしてできる様にした

--- src/lib/usecase/countDownUseCase.ts
+++ src/lib/usecase/countDownUseCase.ts
@@ -1,7 +1,7 @@
 "use client"
 
-export default async function countDownUseCase({setCount}:{setCount:(prop:number)=>void}):Promise<void> {
-  for (let i: number = 3; i > 0; i--) {
+export default async function countDownUseCase({start,setCount}:{start:number,setCount:(prop:number)=>void}):Promise<void> {
+  for (let i: number = start; i > 0; i--) {
     setCount(i);
     await new Promise((resolve) => setTimeout(resolve, 1000));
   }
