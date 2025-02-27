733ad72490f7f7a73a3232639aa77ca9d6b68d58
kmjak <tymkoron1221@icloud.com>
2025-02-28 03:46:33 +0900

    とってくるshapeをShape[][]じゃなくてSahpe[]からとってくるようにした

--- src/lib/usecase/fetchShapeUseCase.ts
+++ src/lib/usecase/fetchShapeUseCase.ts
@@ -3,7 +3,7 @@
 import Shape from "@/types/shape/Shape"
 import generateRandomNumber from "@/utils/generateRandomNumber"
 
-export default async function fetchShapeUseCase({candidateShapes}:{candidateShapes:Shape[][]}):Promise<Shape> {
+export default async function fetchShapeUseCase({candidateShapes}:{candidateShapes:Shape[]}):Promise<Shape> {
   const fetchShapeNumber = await generateRandomNumber({min:0,max:4});
-  return candidateShapes[fetchShapeNumber][0];
+  return candidateShapes[fetchShapeNumber];
 }
